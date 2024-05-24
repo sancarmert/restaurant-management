54f8b95487bcb252b80ba76d4b5899221390d349
Mert Sancar <msmertsancar@gmail.com>
2024-05-24 15:11:25 +0300

    first commit

---
+++ routes/userRouter.go
@@ -0,0 +1,12 @@
+pacakge routes
+
+import(
+
+)
+
+func UserRoutes(incomingRoutes*gin.Engine){
+	incomingRoutes.GET("/users",controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+	incomingRoutes.POST("/users/signup",controller.SignUp())
+	incomingRoutes.POST("/users/login",controller.Login())
+}
