1076a4ecc6285ee68799a01d0ea02b3e48126bc4
Mert Sancar <msmertsancar@gmail.com>
2024-05-29 17:46:42 +0300

    func()'s added

---
+++ controllers/foodController.go
@@ -0,0 +1,37 @@
+package controllers
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetFoods() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+func GetFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+func CreateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+func round(num float64) int {
+
+}
+func toFixed(num float64, precision int) float64 {
+
+}
+func inTimeSpan(start, end, check time.Time) bool {
+
+}
+
+func UpdateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
---
+++ controllers/orderController.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"restaurant-management/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetOrders() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func UpdateOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func OrderItemOrderCreator(order models.Order) string {
+
+}
