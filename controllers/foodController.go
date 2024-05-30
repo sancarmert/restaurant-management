55e289fa378fd7790bd2dc8bde699ddf40faa022
Mert Sancar <msmertsancar@gmail.com>
2024-05-30 21:32:00 +0300

    GetFood() added

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,10 +1,20 @@
 package controllers
 
 import (
+	"context"
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
+	"github.com/sancarmert/restaurant-management/database"
+	"github.com/sancarmert/restaurant-management/models"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
 )
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+var validate = validator.New()
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -12,7 +22,16 @@ func GetFoods() gin.HandlerFunc {
 }
 func GetFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		foodId := c.Param("food_id")
+		var food models.Food
 
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the food item"})
+		}
+		c.JSON(http.StatusOK, food)
 	}
 }
 func CreateFood() gin.HandlerFunc {
